62aa81e835b7b74d0c45654afabcf8146b83d288
MackeyK24 <MackeyK24@gmail.com>
2026-08-30 00:02:42 -1000

    Babylon Toolkit Update 9.22.2

--- Editor/GithubCopilot.cs
+++ Editor/GithubCopilot.cs
@@ -10,12 +10,12 @@ public class AI_PairProgrammer : EditorWindow
     public const string YOUR_COPILOT_DASHBOARD = "https://github.com/settings/copilot";
     public const string YOUR_COPILOT_EXTENSION = "https://marketplace.visualstudio.com/items?itemName=MackeyKinard.codewrx-babylon-agent";
 
-    [MenuItem("Tools/" + CanvasToolsStatics.CANVAS_TOOLS_MENU + "/Developer Options/Your Copilot Dashboard", false, 72)]
+    [MenuItem("Tools/" + CanvasToolsStatics.CANVAS_TOOLS_MENU + "/Developer Options/Your Copilot Dashboard", false, 82)]
     static void AI_GithubCopilot()
     {
         Application.OpenURL(AI_PairProgrammer.YOUR_COPILOT_DASHBOARD);
     }
-    [MenuItem("Tools/" + CanvasToolsStatics.CANVAS_TOOLS_MENU + "/Developer Options/Install Copilot Chat Agent", false, 73)]
+    [MenuItem("Tools/" + CanvasToolsStatics.CANVAS_TOOLS_MENU + "/Developer Options/Install Copilot Chat Agent", false, 83)]
     static void AI_CopilotAgent()
     {
         Application.OpenURL(AI_PairProgrammer.YOUR_COPILOT_EXTENSION);
